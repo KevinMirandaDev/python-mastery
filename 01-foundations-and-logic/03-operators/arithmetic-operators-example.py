8b0b8285baafe1af10761487556199e544234eb8
Kevin <kevinjesusmirandacastro@gmail.com>
2026-02-06 18:43:45 -0500

    Uploading examples...

---
+++ 01-foundations-and-logic/03-operators/arithmetic-operators-example.py
@@ -0,0 +1,29 @@
+# Arithmetic operators
+
+## Addition
+my_addition = 3 + 2   #Result: 5
+print(my_addition)
+
+## Subtraction
+my_subtraction = 5 - 3 #Result: 2
+print(my_subtraction)
+
+## Multiplication
+my_multiplication = 3 * 2  #Result: 6
+print(my_multiplication)
+
+## Division
+my_division = 6/2 #Result: 3.0
+print(my_division)
+
+## Floor Division
+my_floordivision = 10//3 #Result: 3
+print(my_floordivision)
+
+## Modulo
+my_modulo = 10%3 #Result: 1
+print(my_modulo)
+
+## Power
+my_power = 8**2 # 8^2. Result: 64
+print(my_power)
---
+++ 01-foundations-and-logic/03-operators/logic-operators-example.py
@@ -0,0 +1,14 @@
+#Logic Operators
+
+## AND
+an_and = True and False #Result: False
+print(an_and)
+
+
+## OR
+an_or = True or False #Result: True
+print(an_or)
+
+## NOT
+a_not = not True #Result: False
+print(a_not)
