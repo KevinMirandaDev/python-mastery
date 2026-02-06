0fdd1550baa0f5ba2cb914d03bed154fa4b8d518
Kevin <kevinjesusmirandacastro@gmail.com>
2026-02-06 12:23:42 -0500

    Adding examples...

---
+++ 01-Foundations-and-Logic/[01-03]-Data-Structures-Overview/01-03-example.py
@@ -0,0 +1,24 @@
+#List
+my_list = ["Kevin", "KevinMirandaDev", True, 1.75]
+print(my_list)
+
+#Tuple
+my_tuple = ("Kevin", "KevinMirandaDev", True, 1.75)
+print(my_tuple)
+
+#Set
+my_set = {"Kevin", "KevinMirandaDev", True, 1.75}
+print(my_set)
+
+
+#Dictionary
+my_dictionary = {
+    'name' : "Kevin",
+    'github' : "KevinMirandaDev",
+    'is_active' : True,
+    'height' : 1.75
+}
+print(my_dictionary)
+
+
+#Soli Deo Gloria
