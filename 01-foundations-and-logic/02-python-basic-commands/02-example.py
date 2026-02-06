b28548e0543ed839d515c39809af8a75bf6e48d9
Kevin <kevinjesusmirandacastro@gmail.com>
2026-02-06 18:45:58 -0500

    Uploading examples...

---
+++ 01-foundations-and-logic/02-python-basic-commands/02-example.py
@@ -0,0 +1,23 @@
+greetings = "Hello World!" #Variable
+
+#Data types
+text = "Hello" #String (str)
+number = 4     #Integer (int)
+pi = 3.14      #Float (float)
+is_active = True #Boolean (bool)
+Result = None   #None (NoneType)
+
+
+#This is a comment
+
+#print()
+print ("Hello World!") 
+
+
+#f-strings
+name = "Kevin"
+print (f"Hello World! my name is {name}") 
+
+
+#input
+user_text = input("Write your text right here: ")
